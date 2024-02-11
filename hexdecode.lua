39b38bb1158ed45f72bbc32a86932031c0e98814
Niels Voss <code@nielsvoss.com>
2024-02-11 02:33:47 -0800

    Start decoding individual iotas

--- hexdecode.lua
+++ hexdecode.lua
@@ -19,3 +19,33 @@ function HexDecode.decode_pattern(pattern)
 
     return 'Unknown ('..angles..')'
 end
+
+function HexDecode.decode_nonlist(iota)
+    if type(iota) == 'number' then
+        return '- '..tostring(iota)
+    elseif type(iota) == 'boolean' then
+        return '- '..tostring(iota)
+    elseif type(iota) == 'string' then
+        return '- "'..iota..'"'
+    elseif iota.angles then
+       return HexDecode.decode_iota(iota)
+    elseif iota.null then
+        return "- null"
+    elseif iota.garbage then
+        return "- garbage"
+    elseif iota.x and iota.y and iota.z then
+        return '<'..tostring(iota.x)..', '..tostring(iota.y)..', '..tostring(iota.z)..'>'
+    elseif iota.uuid then
+        return '- garbage // Entities are currently unsupported by the decoder'
+    elseif iota.iotaType then
+        return '- garbage // Iota Types are currently unsupported by the decoder'
+    elseif iota.gate then
+        return '- garbate // Gates are currently unsupported by the decoder'
+    elseif iota.moteUuid then
+        return '- garbage // Motes are currently unsupported by the decoder'
+    elseif iota.matrix then
+        return '- garbage // Matricies are currently unsuppored by the decoder'
+    else
+        return '- garbage // Unrecognized Iota Type'
+    end
+end
