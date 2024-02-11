2d50c25a7cb9cfc82831a4a753f8cae9bdfc66cb
Niels Voss <code@nielsvoss.com>
2024-02-11 03:09:40 -0800

    Create HexConvert.decompile_to_lines

--- hexconvert.lua
+++ hexconvert.lua
@@ -1,5 +1,6 @@
 require('hexparsing')
 require('hexprocessing')
+require('hexdecode')
 
 HexConvert = {}
 
@@ -26,6 +27,16 @@ function HexConvert.compile(s)
    local ast = HexParsing.tokens_to_ast(tokens)
    local iotas = HexProcessing.process(ast)
    return iotas
+end
+
+function HexConvert.decompile_to_lines(iota)
+   if type(iota) ~= 'table' or (iota ~= {} and not iota[1]) then
+      local lines = HexDecode.decode_iotas({iota})
+      table.insert(lines, 1, "// Warning: this Iota was originally not a list, but will be written back as a single element list")
+      return lines
+   else
+      return HexDecode.decode_iotas(iota)
+   end
 end
 
 local sample = [[
