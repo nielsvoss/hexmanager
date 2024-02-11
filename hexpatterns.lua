0243e2d91b686d3dc7183b851abc9d26f791cd58
Niels Voss <code@nielsvoss.com>
2024-02-10 18:46:10 -0800

    Create function to check if a pattern has no static representation

--- hexpatterns.lua
+++ hexpatterns.lua
@@ -12,11 +12,16 @@ for fields in f:lines() do
     table.insert(pattern_table, fields)
 end
 
-function HexPatterns.pattern_from_translation(name)
+function HexPatterns.from_translation(name)
     for _,pattern in ipairs(pattern_table) do
         if pattern.translation == name then
             return pattern
         end
     end
     return nil
+end
+
+function HexPatterns.is_dynamic(pattern)
+    -- The == 'true' is necessary because pattern.is_great is a string and is still true when 'false'
+    return pattern.is_great == 'true' or pattern.pattern == ''
 end
