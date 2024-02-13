9255a4c437c63722c9644fe02c2950d931ce2791
Niels Voss <code@nielsvoss.com>
2024-02-12 22:29:47 -0800

    Create addprogramstopath.lua

---
+++ addprogramstopath.lua
@@ -0,0 +1,8 @@
+local path = "/hexconvert/programs"
+if  shell.path():match(path) then
+    print("Shell path already contains "..path)
+else
+    local new_path = shell.path()..":"..path
+    shell.setPath(new_path)
+    print("Added "..path.." to shell path")
+end
