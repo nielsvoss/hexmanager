19e83a19b06216fcfc2b28851c37844c6d2ac51c
Niels Voss <code@nielsvoss.com>
2024-02-24 23:24:41 -0800

    Start working on filtering selections by typing

--- selectionscreen.lua
+++ selectionscreen.lua
@@ -5,8 +5,9 @@ local down_arrow_key = 264
 local right_arrow_key = 262
 local left_arrow_key = 263
 local enter_key = 257
+local backspace_key = 259
 
-local function draw_menu(menu_items, current_selection, index_at_top, max_number_to_display)
+local function draw_menu(menu_items, current_selection, index_at_top, max_number_to_display, header)
     term.clear()
 
     local number_to_display = math.min(max_number_to_display, #menu_items - index_at_top + 1)
@@ -25,22 +26,35 @@ local function draw_menu(menu_items, current_selection, index_at_top, max_number
     end
 
     term.setCursorPos(1, 1)
+    term.write(header)
 end
 
-function SelectionScreen.menu(menu_items)
-    local current_selection = 1
-    local index_at_top = 1
-
+function SelectionScreen.menu(menu_items, prompt)
     local _, height = term.getSize()
     local number_to_display = height - 1
 
-    draw_menu(menu_items, current_selection, index_at_top, number_to_display)
-    local sEvent, nKey = os.pullEvent('key')
-    while nKey ~= enter_key do
-        if nKey == down_arrow_key then
-            current_selection = math.min(current_selection + 1, #menu_items)
-        elseif nKey == up_arrow_key then
-            current_selection = math.max(current_selection - 1, 1)
+    local current_selection = 1
+    local index_at_top = 1
+    local filter_text = ''
+
+    while true do
+        draw_menu(menu_items, current_selection, index_at_top, number_to_display, prompt..filter_text)
+        local event, p1 = os.pullEvent()
+
+        if event == 'key' then
+            if p1 == enter_key then
+                break
+            elseif p1 == backspace_key then
+                if #filter_text > 0 then
+                    filter_text = filter_text:sub(1, #filter_text - 1)
+                end
+            elseif p1 == down_arrow_key then
+                current_selection = math.min(current_selection + 1, #menu_items)
+            elseif p1 == up_arrow_key then
+                current_selection = math.max(current_selection - 1, 1)
+            end
+        elseif event == 'char' then
+            filter_text = filter_text..p1
         end
 
         -- Scroll screen up and down
@@ -50,10 +64,6 @@ function SelectionScreen.menu(menu_items)
         elseif current_selection > index_at_bottom then
             index_at_top = current_selection - number_to_display + 1
         end
-
-        draw_menu(menu_items, current_selection, index_at_top, number_to_display)
-
-        sEvent, nKey = os.pullEvent('key')
     end
 
     term.clear()
@@ -64,4 +74,4 @@ local menu_items = {}
 for i=1,20 do
     table.insert(menu_items, i)
 end
-print(SelectionScreen.menu(menu_items))
+print(SelectionScreen.menu(menu_items, "select: "))
